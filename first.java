d40163255c744924a98b735495d5210a060246e4
sai kumar <saikumar36601@gmail.com>
2021-10-01 18:34:30 +0530

    java

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first
+{
+    public static void main (String[] args){
+        int a =5;
+        System.out.println("hello world "+a);
+    }
+}
---
+++ second.java
@@ -0,0 +1,9 @@
+public class second {
+    public static void main(String[] args){
+        int a=7;
+        int b=8;
+        int c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
